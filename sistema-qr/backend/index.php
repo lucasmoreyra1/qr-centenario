330f573532178174c328f2d77dfe74f014c67580
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2024-09-17 23:40:18 -0300

    err

--- sistema-qr/backend/index.php
+++ sistema-qr/backend/index.php
@@ -9,7 +9,7 @@
         // Verificar si la carpeta existe
         if (!is_dir($rutaCarpeta)) {
             // Si la carpeta no existe, incluir los archivos
-            // require 'read.php';
+            require 'read.php';
             require 'generar_qr.php';
         }
     }
